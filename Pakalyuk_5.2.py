1f2c6fae57ba873ed55a82a5f8e12f3440914c95
Nikita_P <Nikitap10@mail.ru>
2021-04-25 18:25:04 +0300

    lesson-5

---
+++ Pakalyuk_5.2.py
@@ -0,0 +1,3 @@
+n = 10
+for elements in range(1, n + 1, 2):
+    print(elements)
---
+++ Pakalyuk_5.3.py
@@ -0,0 +1,29 @@
+def gen(items1, items2):
+        k = len(items1) - len(items2)
+        if k > 0:
+            for _ in range(k):
+                items2.append(None)
+        for tutor, klass in zip(items1, items2):
+            yield (tutor, klass)
+
+
+tutors = [
+    'Иван', 'Анастасия', 'Петр', 'Сергей',
+    'Дмитрий', 'Борис', 'Елена', 'Светлана', 'Георгий'
+]
+klasses = [
+    '9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А'
+]
+
+my_gener = gen(tutors, klasses)
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
+print(next(my_gener))
